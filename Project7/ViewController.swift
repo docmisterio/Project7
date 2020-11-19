116b2d393b180dd066de19a503498c1ff36b9b18
Zach Becker <doc.misterio@gmail.com>
2020-11-19 14:10:12 -0600

    Project 7: Challenge 2. Complete

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -2,11 +2,13 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "credits", style: .plain , target: self, action: #selector(creditsTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "filter", style: .plain , target: self, action: #selector(filterTapped))
         
         let urlString: String
         
@@ -15,10 +17,11 @@ class ViewController: UITableViewController {
         } else {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
-            
+        
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                filteredPetitions = petitions
                 return
             }
         }
@@ -39,14 +42,18 @@ class ViewController: UITableViewController {
             tableView.reloadData()
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        if !filteredPetitions.isEmpty {
+            return filteredPetitions.count
+        } else {
+            return petitions.count
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -62,5 +69,31 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Credits", message: "Data Provided by 'We the People API' from the White House", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    @objc func filterTapped() {
+        let ac = UIAlertController(title: "Filter by:", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let filterSubmit = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let termEntered = ac?.textFields?[0].text else { return }
+            self?.submitFilterTerm(term: termEntered)
+        }
+        ac.addAction(filterSubmit)
+        present(ac, animated: true)
+    }
+    
+    func submitFilterTerm(term: String) {
+        if term.isEmpty { return }
+        
+        filteredPetitions.removeAll(keepingCapacity: true)
+        for petition in petitions {
+            if petition.title.contains(term) {
+                filteredPetitions.append(petition)
+                print(filteredPetitions)
+                tableView.reloadData()
+            }
+        }
     }
 }
